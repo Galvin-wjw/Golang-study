0ce724952d67585330a19684e29762d5f336c261
Galvin <137606834@qq.com>
2019-03-04 20:55:00 +0800

    bs

--- Algorithm/binary-search.go
+++ Algorithm/binary-search.go
@@ -26,6 +26,7 @@ func binary_search(arr[] int,value int) int {
 	return -1
 }
 
+//递归
 func binary_search_recursion(arr[] int,low,high,value int)  int{
 	lenth := len(arr)
 	if low > high || lenth == 0{
@@ -44,9 +45,58 @@ func binary_search_recursion(arr[] int,low,high,value int)  int{
 }
 
 
+//二分查找，第一个等于n的数字
+func bsearch_fisrt_No(arr[] int,value int ) int  {
+	lenth := len(arr)
+	low := 0
+	high := lenth - 1
+
+	for(low <= high){
+		mid := low + (high - low) >> 1
+		if  arr[mid] > value{
+			high = mid - 1
+		}else if arr[mid] < value{
+			low = mid +1
+		}else {
+			if (mid == 0 || arr[mid -1] != value) {
+				return mid
+			}else {
+				high = mid - 1
+			}
+		}
+
+	}
+	return -1
+}
+
+//最后一个等于n的数字
+func bsearch_last_No(arr[] int,value int) int {
+	lenth := len(arr)
+	low := 0
+	high := lenth - 1
+
+	for (low <= high) {
+		mid := low + (high - low) >> 1
+		if arr[mid] > value {
+			high = mid - 1
+		}else if arr[mid] < value{
+			low = mid + 1
+		}else {
+			if(mid == lenth - 1 || arr[mid + 1] != value){
+				return mid
+			}else {
+				low = mid + 1
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
-	var arr = []int{2,2,3,4,5,6,7,8,9,10}
+	var arr = []int{2,2,3,4,5,6,7,8,8,8,8,8,9,10}
 	value := binary_search(arr,2)
 	value2 := binary_search_recursion(arr,0,8,3)
-	fmt.Println(value,value2)
+	value3 := bsearch_fisrt_No(arr,2)
+	value4 := bsearch_last_No(arr,8)
+	fmt.Println(value,value2,value3,value4)
 }
